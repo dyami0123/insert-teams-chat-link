f81318ff3af83cdc43147130eddad5e25ce25408
Dyami0123 <dyamibash@gmail.com>
2024-10-26 13:28:28 -0700

    feat: create insert teams chat plugin

--- main.ts
+++ main.ts
@@ -1,134 +1,114 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, moment, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class insertTeamsChatLink extends Plugin {
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// This adds an editor command that prompts for a link and inserts it
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'teams-chat-link',
+			name: 'Insert Teams Chat Link',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+				this.promptForLink(editor);
+			},
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
+	// Function to prompt for link input and insert into editor
+	promptForLink(editor: Editor) {
+		const promptModal = new parseTeamsLinkModal(this.app, (userInput: string) => {
+			// Insert the link into the editor at the current cursor position
 
-	}
+			let formattedLink = `[Teams Chat](${userInput})`;
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+			editor.replaceRange(formattedLink, editor.getCursor());
+		});
+		promptModal.open();
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
+// PromptModal class to handle input dialog
+class parseTeamsLinkModal extends Modal {
+	constructor(app, onSubmit) {
+	  super(app);
+	  this.onSubmit = onSubmit;
+	}
+  
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	  const { contentEl } = this;
+	  contentEl.createEl('h2', { text: 'Enter A Link to a Teams Chat' });
+  
+	  // Create a text input box
+	  const input = contentEl.createEl('input', { type: 'text' });
+	  input.addEventListener('keydown', (event) => {
+		if (event.key === 'Enter') {
+			this.close();
+			if (this.verifyLink(input.value)) {
+				let chatId = this.extractChatId(input.value);
+				let chatType = this.extractChatType(input.value);
+
+				console.log(`Chat ID: ${chatId}`);
+				console.log(`Chat Type: ${chatType}`);
+
+				if (!chatId || !chatType) {
+					new Notice("Invalid link. Please enter a valid Teams chat link.");
+					return
+				}
+				let chatLink = this.createChatLink(chatId, chatType);
+				console.log(`Chat Link: ${chatLink}`);
+				this.onSubmit(chatLink);
+			} else {
+				new Notice("Invalid link. Please enter a valid Teams chat link.");
+			}
+		}
+	  });
+  
+	  input.focus(); // Auto-focus the input box
+	}
+	
+	verifyLink(link: string): boolean {
+
+		// https://teams.microsoft.com/l/message/19:2408b794-562f-4cbd-9fcc-d552bf662e4a_28dfec61-0222-4d98-a68f-c887adc08855@unq.gbl.spaces/1729895093786?context=%7B%22contextType%22%3A%22chat%22%7D
+		// things to check for:
+		// - starts with https://teams.microsoft.com/l/
+		// the element after com/l/xxx/ begins with "19:..."
+		// if the link contains @thread.v2 that is the stopping point of the chat id (group message)
+		// if the link contains @unq.gbl.spaces that is the stopping point of the chat id (private message)
+		let verificationRegex = /^https:\/\/teams\.microsoft\.com\/l\/message\/19:[a-zA-Z0-9-]+(?:@thread\.v2|@unq\.gbl\.spaces)?/;
+		return verificationRegex.test(link);
 	}
 
+	extractChatId(link: string): string | null {
+		let chatIdRegex = /19:([a-zA-Z0-9_-]+)(?:@thread\.v2|@unq\.gbl\.spaces)?/;
+		let match = chatIdRegex.exec(link);
+		if (match) {
+			return `19:${match[1]}`;
+		} else {
+			return null;
+		}
+	}
+
+	extractChatType(link: string): string | null {
+		//https://teams.microsoft.com/l/message/19:2408b794-562f-4cbd-9fcc-d552bf662e4a_de95db53-8b6e-4200-bdf2-0b715c64cf68@unq.gbl.spaces/1729888448417?context=%7B%22contextType%22%3A%22chat%22%7D
+		// the link above should be valid
+		let chatTypeRegex = /19:[a-zA-Z0-9-]+(?:_[a-zA-Z0-9-]+)?(@thread\.v2|@unq\.gbl\.spaces)/;
+		let match = chatTypeRegex.exec(link);
+		console.log(`Link: ${link}`);
+		console.log(`Match: ${match}`);
+		if (match) {
+			console.log(`Match[1]: ${match[1]}`);
+			return match[1];
+		} else {
+			return null;
+		}
+	}
+
+	createChatLink(chatId: string, chatType: string): string {
+		return `https://teams.microsoft.com/l/chat/${chatId}${chatType}`;
+	}
+	
 	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	  const { contentEl } = this;
+	  contentEl.empty();
 	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
+  }
